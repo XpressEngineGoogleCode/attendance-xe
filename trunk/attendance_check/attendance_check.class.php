253e3d6ca87bc56c1010b6a0eb575f58528fe7fd
pppljh@gmail.com <pppljh@gmail.com@bcb7d73e-de6c-83ec-f12b-efdd23648e53>
2012-05-08 11:42:50 +0000

    1. 출석금지시간 정보 노출하도록 수정 2. 버전번호 수정 1.6.2

--- trunk/attendance_check/attendance_check.class.php
+++ trunk/attendance_check/attendance_check.class.php
@@ -4,7 +4,7 @@ class attendance_check extends WidgetHandler{
 
     function proc($args){
 
-                     /* 템플릿 파일에서 사용할 변수들을 세팅 */
+        /* 템플릿 파일에서 사용할 변수들을 세팅 */
         $obj->title = $args->title;
 
         //attendance model 객체 로드
@@ -12,9 +12,9 @@ class attendance_check extends WidgetHandler{
 
         $selected_date = Context::get('choose_date');
         if(!$selected_date){ $selected_date = zDate(date('YmdHis'),"Ymd");}
-            $obj->start_num = $oAttendanceModel->getPositionData($selected_date,"^showall^");
+        $obj->start_num = $oAttendanceModel->getPositionData($selected_date,"^showall^");
 
-                      /*각종 포인트 설정값 꺼내오기*/
+        /*각종 포인트 설정값 꺼내오기*/
         $output = executeQuery('attendance.getConfigData');
         $config_data = $output->data;
 
@@ -22,16 +22,16 @@ class attendance_check extends WidgetHandler{
         $oModuleModel = &getModel('module');
         $config = $oModuleModel->getModuleConfig('attendance');
         if(!$config->about_admin_check)$config->about_admin_check = 'yes';
-            Context::set('config2',$config);
+        Context::set('config2',$config);
 
-                      //포인트 변수
+        //포인트 변수
         $point = $config_data->add_point;
 
-                      //리스트 변수
+        //리스트 변수
         if(!$args->list_count)
             $args->list_count = 5;
 
-                     //변수 보내기
+        //변수 보내기
         $obj->position = $args->position;
         $obj->reg_time = $args->reg_time;
         $obj->nick_name = $args->nick_name;
@@ -60,20 +60,20 @@ class attendance_check extends WidgetHandler{
         else 
             $obj->greetings_length = $args->greetings_length;
 
-                      /*연속출석 관련 설정*/
+        /*연속출석 관련 설정*/
         $continuity->about_continuity = $config_data->about_continuity;
         $continuity->continuity_day = $config_data->continuity_day;
 
-                     //지정일 포인트 설정
+        //지정일 포인트 설정
         $obj->about_target = $config_data->about_target;
         $obj->target_day = $config_data->target_day;
 
-                      //등수포인트 초기화
+        //등수포인트 초기화
         $obj->add_1st = $config_data->first_point;
         $obj->add_2nd = $config_data->second_point;
         $obj->add_3rd = $config_data->third_point;
 
-                     //출석했는지 여부 알아보기		
+        //출석했는지 여부 알아보기		
         $flag = 1;
         $logged_info = Context::get('logged_info');
         if($oAttendanceModel->getIsChecked($logged_info->member_srl) == 0)
@@ -81,18 +81,18 @@ class attendance_check extends WidgetHandler{
         else
             $flag = 1;
 
-                      //포인트 모듈 연동
+        //포인트 모듈 연동
         $oPointModel = &getModel('point');
 
         //member 모듈 연동
         $oMemberModel = &getModel('member');
 
-                      //출석가능 시간대인지 판단
+        //출석가능 시간대인지 판단
         $is_available = $oAttendanceModel->availableCheck($config_data);
 
         $oDocumentModel = &getModel('document');
-
-                      //변수 내보내기
+        
+        //변수 내보내기
         Context::set('selected_date',$selected_date);
         Context::set('continuity',$continuity);
         Context::set('add_point',$point);
@@ -107,22 +107,22 @@ class attendance_check extends WidgetHandler{
         Context::set('oMemberModel',$oMemberModel);
         Context::set('oDocumentModel',$oDocumentModel);
 
-                     //모바일 페이지용 각종 스크립트 로드
+        //모바일 페이지용 각종 스크립트 로드
         if($args->mobile_skin == 'yes'){
             Context::addJsFile("./common/js/jquery.js", true, '', -100005);
             Context::addJsFile("./common/js/js_app.js", true, '', -100004);
             Context::addJsFile("./common/js/common.js", true, '', -100003);
             Context::addJsFile("./common/js/xml_handler.js", true, '', -100002);
             Context::addJsFile("./common/js/xml_js_filter.js", true, '', -100001);
-                      }
+        }
 
-                     // 템플릿의 스킨 경로를 지정 
+        // 템플릿의 스킨 경로를 지정 
         $tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
         $tpl_file = 'body';
 
-                      // 템플릿 컴파일
+        // 템플릿 컴파일
         $oTemplate = &TemplateHandler::getInstance();
         return $oTemplate->compile($tpl_path, $tpl_file);
-        }
+    }
 }
 ?>
